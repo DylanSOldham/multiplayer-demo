df451f987b828f8c43ec5828234f4346df932d36
DylanSOldham <dylansoldham@gmail.com>
2024-03-12 15:17:04 -0700

    Added attack and dodge actions

--- public/index.js
+++ public/index.js
@@ -2,18 +2,20 @@
 // Global Constants
 //---------------------------------------------------------------------------------------
 
+const SERVER_URL = "ws://localhost:8081";
+
 const AVATAR_SIZE = 5;
 const AVATAR_MAX_HEALTH = 100;
 const AVATAR_SPEED = 2;
-const AVATAR_DODGE_SPEED = 5;
+const AVATAR_DODGE_SPEED = 0;
 const WORLD_SIZE = 150;
 const ATTACK_DURATION = 0.1; // Seconds
-const DODGE_DURATION = 0.3; // Seconds
-const ATTACK_COOLDOWN = 0.5; // Seconds
-const DODGE_COOLDOWN = 0.5; // Seconds
-
-
-
+const ATTACK_COOLDOWN = 0.2; // Seconds
+const ATTACK_MIN_RADIUS = 1.0;
+const ATTACK_MAX_RADIUS = 10.0;
+const ATTACK_ANGLE_WIDTH = 2 * Math.PI / 3;
+const DODGE_DURATION = 1.0; // Seconds
+const DODGE_COOLDOWN = 0.2; // Seconds
 
 
 //---------------------------------------------------------------------------------------
@@ -24,11 +26,21 @@ const kbd = {}; // Keyboard
 const mse = {}; // Mouse
 
 const refreshKeys = () => {
-  mse["just_left"] = false;
-  mse["just_right"] = false;
+    mse["just_left"] = false;
+    mse["just_right"] = false;
 }
 
+const updateMousePos = event => {
+    // Conversion from client coordinates to canvas coordinates 
+    // adapted from the top answer here https://stackoverflow.com/questions/17130395/real-mouse-position-in-canvas
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
 
+    const pos = screen2world(x, y);
+    mse.x = pos.x;
+    mse.y = pos.y;
+}
 
 
 
@@ -37,63 +49,60 @@ const refreshKeys = () => {
 //---------------------------------------------------------------------------------------
 
 const canvas = document.querySelector("canvas");
-canvas.width = canvas.clientWidth;
-canvas.height = canvas.clientHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
+window.addEventListener("resize", event => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+})
+
 canvas.addEventListener("keydown", event => {
-  kbd[event.code] = true;
+    kbd[event.code] = true;
 });
 
 canvas.addEventListener("keyup", event => {
-  kbd[event.code] = false;
+    kbd[event.code] = false;
 });
 
 canvas.addEventListener("mousemove", event => {
-  // Conversion from client coordinates to canvas coordinates 
-  // adapted from the top answer here https://stackoverflow.com/questions/17130395/real-mouse-position-in-canvas
-  const rect = canvas.getBoundingClientRect();
-  const x = event.clientX - rect.left;
-  const y = event.clientY - rect.top;
-
-  const pos = screen2world(x, y);
-  mse.x = pos.x;
-  mse.y = pos.y;
+    updateMousePos(event);
 });
 
 canvas.addEventListener("mousedown", event => {
-  switch (event.button)
-  {
-    case 0:
-      mse["left"] = true;
-      mse["just_left"] = true;
-      break;
-    case 2:
-      mse["right"] = true;
-      mse["just_right"] = true;
-      break;
-    default:
-      break;
-  }
+    updateMousePos(event);
+
+    switch (event.button) {
+        case 0:
+            mse["left"] = true;
+            mse["just_left"] = true;
+            break;
+        case 2:
+            mse["right"] = true;
+            mse["just_right"] = true;
+            break;
+        default:
+            break;
+    }
 });
 
 canvas.addEventListener("mouseup", event => {
-  switch (event.button)
-  {
-    case 0:
-      mse["left"] = true;
-      break;
-    case 2:
-      mse["right"] = true;
-      break;
-    default:
-      break;
-  }
+    switch (event.button) {
+        case 0:
+            mse["left"] = true;
+            break;
+        case 2:
+            mse["right"] = true;
+            break;
+        default:
+            break;
+    }
 });
 
 canvas.addEventListener("contextmenu", e => {
-  e.stopPropagation(); 
-  e.preventDefault();
+    e.stopPropagation();
+    e.preventDefault();
 });
 
 
@@ -104,32 +113,33 @@ canvas.addEventListener("contextmenu", e => {
 // Camera
 //---------------------------------------------------------------------------------------
 const camera = {
-  x: 0,
-  y: 0,
-  size: 100,
+    x: 0,
+    y: 0,
+    size: 100,
 };
 
 const world2screen = (x, y, w, h) => {
-  return {
-    x: (x - camera.x + camera.size / 2) * canvas.height * (4/3) / camera.size,
-    y: (y - camera.y + camera.size / 2) * canvas.height / camera.size,
-    w: w * canvas.height * (4/3) / camera.size,
-    h: h * canvas.height / camera.size,
-  };
+    return {
+        x: (x - camera.x + camera.size / 2) * canvas.width / camera.size,
+        y: (y - camera.y + camera.size / 2) * canvas.height / camera.size,
+        w: w * canvas.width / camera.size,
+        h: h * canvas.height / camera.size,
+    };
 };
 
 const screen2world = (x, y, w, h) => {
-  return {
-    x: x / (canvas.height * (4/3) / camera.size) + camera.x - camera.size / 2,
-    y: y / (canvas.height / camera.size) + camera.y - camera.size / 2 ,
-    w: w / (canvas.height * camera.size),
-    h: h / (canvas.height * camera.size),
-  };
+    return {
+        x: x / (canvas.width / camera.size) + camera.x - camera.size / 2,
+        y: y / (canvas.height / camera.size) + camera.y - camera.size / 2,
+        w: w / (canvas.width * camera.size),
+        h: h / (canvas.height * camera.size),
+    };
 };
 
-const updateCamera = avatar => {
-  camera.x = avatar.x;
-  camera.y = avatar.y;
+const updateCamera = () => {
+    const avatar = avatars[playerAvatarId];
+    camera.x = avatar.x;
+    camera.y = avatar.y;
 };
 
 
@@ -141,24 +151,24 @@ const updateCamera = avatar => {
 //---------------------------------------------------------------------------------------
 
 const drawRect = (x, y, w, h, style) => {
-  const body = world2screen(x, y, w, h);
-  ctx.fillStyle = style;
-  ctx.fillRect(body.x, body.y, body.w, body.h);
+    const body = world2screen(x, y, w, h);
+    ctx.fillStyle = style;
+    ctx.fillRect(body.x, body.y, body.w, body.h);
 }
 
 const drawArc = (x, y, r1, r2, startangle, endangle, style) => {
-  const body = world2screen(x, y, r1, r2);
-  r1 = body.w;
-  r2 = body.h;
-  ctx.beginPath();
-  ctx.fillStyle = style;
-  ctx.arc(body.x, body.y, r2, startangle, endangle);
-  ctx.arc(body.x, body.y, r1, endangle, startangle, true);
-  ctx.fill();
+    const body = world2screen(x, y, r1, r2);
+    r1 = body.w;
+    r2 = body.h;
+    ctx.beginPath();
+    ctx.fillStyle = style;
+    ctx.arc(body.x, body.y, r2, startangle, endangle);
+    ctx.arc(body.x, body.y, r1, endangle, startangle, true);
+    ctx.fill();
 }
 
 const drawCircle = (x, y, r, style) => {
-  return drawArc(x, y, 0, r, 0, 2 * Math.PI, style);
+    return drawArc(x, y, 0, r, 0, 2 * Math.PI, style);
 }
 
 
@@ -167,82 +177,83 @@ const drawCircle = (x, y, r, style) => {
 // Avatar
 //---------------------------------------------------------------------------------------
 
-const avatar = {
-  health: 90.0,
-  x: 0,
-  y: 0,
-  angle: 0.0,
-  attack_timer: 100.0,
-  dodge_timer: 100.0,
-  style: "rgba(255, 195, 195, 1.0)",
-};
+let avatars = null;
+let playerAvatarId = null;
 
 const isDodging = avatar => avatar.dodge_timer < DODGE_DURATION;
 const isAttacking = avatar => avatar.attack_timer < ATTACK_DURATION;
 const canDodge = avatar => avatar.dodge_timer > DODGE_DURATION + DODGE_COOLDOWN;
 const canAttack = avatar => avatar.attack_timer > ATTACK_DURATION + ATTACK_COOLDOWN;
 
-const clampAvatarToWorld = avatar => {
-  avatar.x = Math.max(-WORLD_SIZE/2, Math.min(WORLD_SIZE/2, avatar.x));
-  avatar.y = Math.max(-WORLD_SIZE/2, Math.min(WORLD_SIZE/2, avatar.y));
-}
+let doDodge = false;
+const updatePlayerAvatar = () => {
+    const avatar = avatars[playerAvatarId];
 
-const updateAvatar = avatar => { // WIll be replaced by multiplayer state updates
+    let dx = 0;
+    let dy = 0;
 
-  if (kbd["ArrowUp"] || kbd["KeyW"]) avatar.y -= AVATAR_SPEED;
-  if (kbd["ArrowDown"] || kbd["KeyS"]) avatar.y += AVATAR_SPEED;
-  if (kbd["ArrowRight"] || kbd["KeyD"]) avatar.x += AVATAR_SPEED;
-  if (kbd["ArrowLeft"] || kbd["KeyA"]) avatar.x -= AVATAR_SPEED;
+    if (kbd["ArrowUp"] || kbd["KeyW"]) dy -= AVATAR_SPEED;
+    if (kbd["ArrowDown"] || kbd["KeyS"]) dy += AVATAR_SPEED;
+    if (kbd["ArrowRight"] || kbd["KeyD"]) dx += AVATAR_SPEED;
+    if (kbd["ArrowLeft"] || kbd["KeyA"]) dx -= AVATAR_SPEED;
+    if (kbd["Space"]) doDodge = true;
 
-  if (canAttack(avatar) && mse["just_left"]) {
-    avatar.attack_timer = 0.0;
-    avatar.angle = Math.atan2(mse.y - avatar.y, mse.x - avatar.x);
-  }
+    if (canAttack(avatar) && mse["just_left"]) {
+        avatar.angle = Math.atan2(mse.y - avatar.y, mse.x - avatar.x);
+        sendAttack(avatar.angle);
+    }
 
-  if (canDodge(avatar) && mse["just_right"]) {
-    avatar.dodge_timer = 0.0;
-    avatar.angle = Math.atan2(mse.y - avatar.y, mse.x - avatar.x);
-  }
+    if (canDodge(avatar) && mse["just_right"]) {
+        avatar.angle = Math.atan2(mse.y - avatar.y, mse.x - avatar.x);
+        sendDodge(avatar.angle);
+    }
 
-  if (!canAttack(avatar)) avatar.attack_timer += 0.016;
-  if (!canDodge(avatar)) avatar.dodge_timer += 0.016;
+    if (canDodge(avatar) && doDodge)
+    {
+        avatar.angle = avatar.angle ? 0 : Math.PI;
+        sendDodge(avatar.angle);
+    }
 
-  if (isDodging(avatar))
-  {
-    avatar.x += AVATAR_DODGE_SPEED * Math.cos(avatar.angle);
-    avatar.y += AVATAR_DODGE_SPEED * Math.sin(avatar.angle);
-  }
+    if (isDodging(avatar)) {
+        dx += AVATAR_DODGE_SPEED * Math.cos(avatar.angle);
+        dy += AVATAR_DODGE_SPEED * Math.sin(avatar.angle);
+    }
 
-  clampAvatarToWorld(avatar);
+    if (dx != 0 || dy != 0) sendMove(dx, dy);
 }
 
 const drawAttack = avatar => {
-  drawArc(avatar.x, avatar.y, AVATAR_SIZE + 1, AVATAR_SIZE + 10, avatar.angle - Math.PI / 3, avatar.angle + Math.PI / 3, "#FF0000");
+    drawArc(avatar.x, avatar.y, 
+        AVATAR_SIZE + ATTACK_MIN_RADIUS, AVATAR_SIZE + ATTACK_MAX_RADIUS, 
+        avatar.angle - ATTACK_ANGLE_WIDTH / 2, avatar.angle + ATTACK_ANGLE_WIDTH / 2, "#FF0000");
 }
 
-const drawHealthbar = avatar => {
-  ctx.fillStyle = "#000000";
-  ctx.fillRect(5, 5, canvas.width - 10, canvas.width/50);
-  ctx.fillStyle = "#00FF00";
-  ctx.fillRect(5, 5, (avatar.health / AVATAR_MAX_HEALTH) * (canvas.width - 10), canvas.width/50);
+const drawHealthbar = () => {
+    let avatar = avatars[playerAvatarId];
+
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(5, 5, canvas.width - 10, canvas.width / 50);
+    ctx.fillStyle = "#00FF00";
+    ctx.fillRect(5, 5, (avatar.health / AVATAR_MAX_HEALTH) * (canvas.width - 10), canvas.width / 50);
 }
 
 const drawAvatar = avatar => {
-  let color = avatar.style;
-  if (isDodging(avatar))
-  {
-    color = "rgba(255, 195, 195, 0.6)";
-  }
+    let alpha = isDodging(avatar) ? 0.6 : 1.0;
+    let color = `rgba(${avatar.color.r}, ${avatar.color.g}, ${avatar.color.b}, ${alpha})`;
 
-  drawCircle(avatar.x, avatar.y, AVATAR_SIZE, color);
+    drawCircle(avatar.x, avatar.y, AVATAR_SIZE, color);
 
-  if (isAttacking(avatar))
-  {
-    drawAttack(avatar);
-  }
+    if (isAttacking(avatar)) // TODO - Move to separate draw layer
+    {
+        drawAttack(avatar);
+    }
 }
 
-
+const drawAvatars = () => {
+    for (let avatarId in avatars) {
+        drawAvatar(avatars[avatarId]);
+    }
+}
 
 
 //---------------------------------------------------------------------------------------
@@ -250,49 +261,64 @@ const drawAvatar = avatar => {
 //---------------------------------------------------------------------------------------
 
 const clearFrame = () => {
-  ctx.fillStyle = "#021446";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#021446";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-const drawGrass = () => {
-  drawRect(-WORLD_SIZE/2, -WORLD_SIZE/2, WORLD_SIZE, WORLD_SIZE, "#12C956");
-  for (let i = 0; i < 10; ++i) {
-    for (let j = 0; j < 10; ++j) {
-      drawRect(
-        -WORLD_SIZE/2.02 + i * WORLD_SIZE/10, 
-        -WORLD_SIZE/2.02 + j * WORLD_SIZE/10, 
-        WORLD_SIZE/11, WORLD_SIZE/11, 
-        "#023906"
-      );
+let gridAnim = 0.0;
+
+const drawGrid = () => {
+    drawRect(-WORLD_SIZE / 2, -WORLD_SIZE / 2, WORLD_SIZE, WORLD_SIZE, "#12C956");
+
+    gridAnim += 0.03;
+    if (gridAnim > 2 * Math.PI) gridAnim = 0;
+
+    let spacing = 0.2 * Math.sin(gridAnim) + 0.8;
+    const gridSize = 10;
+
+    let fullX = -WORLD_SIZE / 2;
+    let fullY = -WORLD_SIZE / 2;
+    let fullW = (WORLD_SIZE - spacing);
+    let fullH = (WORLD_SIZE - spacing);
+
+    for (let i = 0; i < gridSize; ++i) {
+        for (let j = 0; j < gridSize; ++j) {
+            drawRect(
+                fullX + i * fullW / gridSize + spacing,
+                fullY + j * fullH / gridSize + spacing,
+                fullW / gridSize - spacing, fullH / gridSize - spacing,
+                `#023906`
+            );
+        }
     }
-  }
 }
 
 const sparkles = [];
-for (let i = 0; i < 200; ++i) sparkles.push({x: 50, y: 50});
+for (let i = 0; i < 200; ++i) sparkles.push({ x: 50, y: 50 });
 
 const drawSparkles = () => {
-  for (let i = 0; i < sparkles.length; ++i)
-  {
-    if (Math.random() < 0.1) {
-      sparkles[i].x = avatar.x + Math.random() * WORLD_SIZE * 2 - WORLD_SIZE / 2;
-      sparkles[i].y = avatar.y + Math.random() * WORLD_SIZE * 2 - WORLD_SIZE / 2;
+    const avatar = avatars[playerAvatarId];
+
+    for (let i = 0; i < sparkles.length; ++i) {
+        if (Math.random() < 0.1) {
+            sparkles[i].x = avatar.x + Math.random() * WORLD_SIZE * 2 - WORLD_SIZE / 2;
+            sparkles[i].y = avatar.y + Math.random() * WORLD_SIZE * 2 - WORLD_SIZE / 2;
+        }
+        drawCircle(sparkles[i].x, sparkles[i].y, Math.random(), `rgba(${255 * Math.random()}, 255, ${255 * Math.random()}, 1.0)`);
     }
-    drawCircle(sparkles[i].x, sparkles[i].y, Math.random(), `rgba(${255 * Math.random()}, 255, ${255 * Math.random()}, 1.0)`);
-  }
 }
 
 const updateFrame = () => {
-  updateAvatar(avatar);
-  updateCamera(avatar);
+    updatePlayerAvatar();
+    updateCamera();
 };
 
 const drawFrame = () => {
-  clearFrame();
-  drawSparkles();
-  drawGrass();
-  drawAvatar(avatar);
-  drawHealthbar(avatar);
+    clearFrame();
+    drawSparkles();
+    drawGrid();
+    drawAvatars();
+    drawHealthbar();
 };
 
 
@@ -304,11 +330,57 @@ const drawFrame = () => {
 //---------------------------------------------------------------------------------------
 
 const step = () => {
-  drawFrame();
-  updateFrame();
-  refreshKeys();
-  requestAnimationFrame(step);
+    drawFrame();
+    updateFrame();
+    refreshKeys();
+
+    console.log(canvas.height);
+
+    requestAnimationFrame(step);
 }
 
-// Game Loop Entry Point
-step();
+
+
+
+
+//---------------------------------------------------------------------------------------
+// Server Communication
+//---------------------------------------------------------------------------------------
+
+websocket = new WebSocket(SERVER_URL);
+websocket.addEventListener("open", event => {
+    console.log(`Successfully connected to ${websocket.url}`);
+});
+
+const sendMove = (offX, offY) => {
+    websocket.send(JSON.stringify({ type: "avatarMove", id: playerAvatarId, offset: { x: offX, y: offY } }));
+};
+
+const sendAttack = angle => {
+    websocket.send(JSON.stringify({ type: "avatarAttack", id: playerAvatarId, angle: angle }));
+}
+
+const sendDodge = angle => {
+    websocket.send(JSON.stringify({ type: "avatarDodge", id: playerAvatarId, angle: angle }));
+}
+
+websocket.addEventListener("message", event => {
+    let data = JSON.parse(event.data);
+    console.log(`Received: ${JSON.stringify(data)}`);
+
+    switch (data.type) {
+        case "myConnect":
+            playerAvatarId = data.id;
+            avatars = data.avatars;
+
+            // Game Loop Entry Point
+            step();
+            break;
+        case "ping":
+            websocket.send(`{"id": "${playerAvatarId}", "type": "pong"}`);
+            break;
+        case "updateAvatars":
+            avatars = data.avatars;
+            break;
+    }
+});
--- server.js
+++ server.js
@@ -3,8 +3,224 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static("./public")); //https://expressjs.com/en/starter/static-files.html
+app.use(express.static("./public")); //Line adapted from https://expressjs.com/en/starter/static-files.html
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+
+
+
+
+
+//---------------------------------------------------------------------------------------
+// Global Constants
+//---------------------------------------------------------------------------------------
+
+const AVATAR_SIZE = 5;
+const AVATAR_MAX_HEALTH = 100;
+const AVATAR_SPEED = 2;
+const AVATAR_DODGE_SPEED = 5;
+const WORLD_SIZE = 150;
+const ATTACK_DURATION = 0.1; // Seconds
+const ATTACK_COOLDOWN = 0.2; // Seconds
+const ATTACK_MIN_RADIUS = 1.0;
+const ATTACK_MAX_RADIUS = 10.0;
+const ATTACK_ANGLE_WIDTH = 2 * Math.PI / 3;
+const DODGE_DURATION = 1.0; // Seconds
+const DODGE_COOLDOWN = 0.2; // Seconds
+
+
+
+
+
+
+//---------------------------------------------------------------------------------------
+// Game State
+//---------------------------------------------------------------------------------------
+
+const avatars = {};
+
+const randomColor = () => { return {r: Math.random() * 255, g: Math.random() * 255, b: Math.random() * 255, a: 1.0} }
+
+const clampAvatarToWorld = avatar => {
+  avatar.x = Math.max(-WORLD_SIZE/2, Math.min(WORLD_SIZE/2, avatar.x));
+  avatar.y = Math.max(-WORLD_SIZE/2, Math.min(WORLD_SIZE/2, avatar.y));
+}
+
+
+
+//---------------------------------------------------------------------------------------
+// Client Communication
+//---------------------------------------------------------------------------------------
+
+// Websocket setup adapted from the "Simple server" example at https://www.npmjs.com/package/ws#usage-examples
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8081 });
+
+let openSockets = [];
+
+const initAvatar = (ws, id) => 
+{
+  avatars[id] = {
+    lastActive: Date.now(),
+    health: 100.0,
+    x: 0,
+    y: 0,
+    angle: 0.0,
+    attack_timer: 100.0,
+    dodge_timer: 100.0,
+    color: randomColor(),
+  };
+
+  const initData = {
+    type: "myConnect",
+    id: id,
+    avatars: avatars
+  };
+
+  ws.send(JSON.stringify(initData));
+}
+
+const sendAvatarsUpdate = () => {
+    const data = {
+      type: "updateAvatars",
+      avatars: avatars
+    };
+  
+    for (s of openSockets)
+    {
+      s.send(JSON.stringify(data));
+    }
+}
+
+const disconnectAvatar = (id) => {
+  delete avatars[id];
+  console.log(`Disconnected player: ${id}`);
+}
+
+const handleMove = data =>
+{
+  let avatar = avatars[data.id];
+  avatar.x += data.offset.x;
+  avatar.y += data.offset.y;
+  clampAvatarToWorld(avatar);
+
+  console.log(`Moving ${data.id} by ${data.offset.x}, ${data.offset.y}`);
+}
+
+const handleAttack = data =>
+{
+  let avatar = avatars[data.id];
+  avatar.angle = data.angle;
+  avatar.attack_timer = 0.0;
+
+  const isDodging = avatar => avatar.dodge_timer < DODGE_DURATION;
+
+  for (let id in avatars)
+  {
+    if (id === data.id) continue;
+
+    let other = avatars[id];
+
+    if (isDodging(other)) continue;
+
+    const otherDx = avatar.x - other.x;
+    const otherDy = avatar.y - other.y;
+    const otherDist2 = otherDx * otherDx + otherDy * otherDy;
+
+    maxDist2 = Math.pow(ATTACK_MAX_RADIUS + AVATAR_SIZE, 2);
+
+    if (otherDist2 < maxDist2 + AVATAR_SIZE * AVATAR_SIZE)
+    {
+        const otherAngle = Math.atan2(other.y - avatar.y, other.x - avatar.x);
+        if (Math.abs(otherAngle - avatar.angle) < ATTACK_ANGLE_WIDTH / 2) {
+            other.health -= 5;
+        }
+    }
+  }
+
+  console.log(`Attack by ${data.id}.`)
+}
+
+const handleDodge = data =>
+{
+    let avatar = avatars[data.id];
+    avatar.angle = data.angle;
+    avatar.dodge_timer = 0.0;
+}
+
+wss.on("connection", (ws, req) => {
+  openSockets.push(ws);
+
+  let avatarId = `${req.socket.remoteAddress}:${req.socket.remotePort}`;
+  initAvatar(ws, avatarId);
+
+  ws.on("message", rawdata => {
+    let data = JSON.parse(rawdata.toString("ascii"));
+
+    if (!avatars[data.id]) return;
+
+    switch (data.type)
+    {
+      case "avatarMove":
+        handleMove(data);
+        break;
+      case "avatarAttack":
+        handleAttack(data);
+        break;
+      case "avatarDodge":
+        handleDodge(data);
+      case "pong":
+        avatars[data.id].lastActive = Date.now();
+      break;
+    }
+  });
+
+  ws.on("close", () => {
+    disconnectAvatar(avatarId);
+  });
+});
+
+const CLEANUP_PERIOD = 1 * 1000;
+
+const updateAvatar = avatar => {
+    const canDodge = () => avatar.dodge_timer > DODGE_DURATION + DODGE_COOLDOWN;
+    const canAttack = () => avatar.attack_timer > ATTACK_DURATION + ATTACK_COOLDOWN;
+
+    if (!canAttack(avatar)) avatar.attack_timer += 0.016;
+    if (!canDodge(avatar)) avatar.dodge_timer += 0.016;
+}
+
+const updateAvatars = () => {
+
+    for (id in avatars)
+    {
+        updateAvatar(avatars[id]);
+    }
+
+    sendAvatarsUpdate();
+
+    setTimeout(updateAvatars, 1.6);
+}
+
+const cleanInactive = () => {
+  for (let id in avatars)
+  {
+    for (s of openSockets)
+    {
+      s.send(`{"type": "ping"}`);
+    }
+
+    if (Date.now() - avatars[id].lastActive > 5 * 1000)
+    {
+      disconnectAvatar(id);
+    }
+  }
+
+  setTimeout(cleanInactive, CLEANUP_PERIOD);
+}
+
+setTimeout(cleanInactive, CLEANUP_PERIOD);
+setTimeout(updateAvatars, 10);
